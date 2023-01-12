5f43af43bc62c4ae52545c0cf5d493fdf2cdcaf2
Mason <83597230+xedoink@users.noreply.github.com>
2023-01-12 16:06:16 -0500

    Update malloctest.c

--- malloctest.c
+++ malloctest.c
@@ -9,16 +9,14 @@ int main()
     printf("Enter limit of the text: ");
     scanf("%d",&n);
     
-    /*allocate memory dynamically*/
     text=(char*)malloc(n*sizeof(char));
     
     printf("Enter text: ");
-    scanf(" "); /*clear input buffer*/
+    scanf(" ");
     gets(text);
     
     printf("Inputted text is: %s\n",text);
     
-    /*Free Memory*/
     free(text);
     
     return 0;
